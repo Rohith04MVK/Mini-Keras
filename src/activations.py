181a29624447f7066024047996326b9003db68d7
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-03 06:21:49 +0000

    Added base class to from activations

---
+++ src/activations.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class ActivationFunction:
+    def function(self, x):
+        raise NotImplementedError
+
+    def derivative(self, x, cached_y=None):
+        raise NotImplementedError
