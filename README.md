e184dfb861f812190b021382bc6fe798a12a451a
janaSunrise <warriordefenderz@gmail.com>
2021-04-03 14:25:18 +0530

    Configure the guidelines, and the attributes in __init__.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Mini-Keras
-### AI and DL library in pure numpy
+
+An advanced and lightweight ML and Deep learning library for python.
+
+**NOTE**: We are not affliated with [Keras](https://github.com/keras-team) in any way.
+We're making a lightweight library for ML and Deep learning.
---
+++ mini_keras/__init__.py
@@ -0,0 +1,16 @@
+
+import typing as t
+
+__author__ = "Mini Keras team"
+__email__ = "warriordefenderz@gmail.com"
+__version__ = "0.1.0"
+__license__ = "GPL-3.0 License"
+__copyright__ = "Copyright 2021 Mini Keras team"
+
+__all__: t.Tuple[str, ...] = (
+    "__author__",
+    "__email__",
+    "__version__",
+    "__license__",
+    "__copyright__",
+)
