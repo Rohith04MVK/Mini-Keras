c1d38c568fde06ed0d9720a03dd5f9fcf2738b01
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-04 11:41:13 +0000

    Added doc strings for dense layer

--- mini_keras/layers/dense.py
+++ mini_keras/layers/dense.py
@@ -4,21 +4,36 @@ from mini_keras.abc import BaseLayer
 
 
 class Dense(BaseLayer):
+    """Densely connected layer.
+    Attributes
+    ----------
+    size : int
+        Number of neurons.
+    activation : Activation
+        Neurons' activation's function.
+    cache : dict
+        Cache.
+    weights : numpy.ndarray
+        Weights.
+    biases : numpy.ndarray
+        biases.
+    """
+
     def __init__(self, size, activation):
         super().__init__
         self.size = size
         self.activation = activation
         self.weights = None
-        self.bias = None
+        self.biases = None
         self.cache = {}
 
     def init(self, input_dims):
         self.weights = np.random.randn(self.size, input_dims) * np.sqrt(2 / input_dims)
 
-        self.bias = np.zeros((1, self.size))
+        self.biases = np.zeros((1, self.size))
 
     def forward(self, a_prev, training):
-        z = np.dot(a_prev, self.weights.T) + self.bias
+        z = np.dot(a_prev, self.weights.T) + self.biases
         a = self.activation.f(z)
 
         if training:
@@ -41,10 +56,10 @@ class Dense(BaseLayer):
 
     def update_params(self, dw, db):
         self.weights -= dw
-        self.bias -= db
+        self.biases -= db
 
     def get_params(self):
-        return self.weights, self.bias
+        return self.weights, self.biases
 
     def get_output_dim(self):
         return self.size
