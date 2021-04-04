a50cdc771a11ce681eba8a9588f282a97cbc6cc1
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-04 11:00:37 +0000

    Added forward propagation for  dense layer

--- mini_keras/layers/dense.py
+++ mini_keras/layers/dense.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from mini_keras.abc import BaseLayer
 
+
 class Dense(BaseLayer):
     def __init__(self, size, activation):
         super().__init__
@@ -16,4 +17,11 @@ class Dense(BaseLayer):
 
         self.bias = np.zeros((1, self.size))
 
+    def forward(self, a_prev, training):
+        z = np.dot(a_prev, self.weights.T) + self.bias
+        a = self.activation.f(z)
 
+        if training:
+            self.cache.update({'a_prev': a_prev, 'z': z, 'a': a})
+
+        return a
