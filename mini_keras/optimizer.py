1771067413cf812e87900a59210416143b5702dd
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-04 15:10:36 +0000

    Added gradient descent optimizer

---
+++ mini_keras/optimizer.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from mini_keras import BaseOptimizer
+
+
+class GradientDescent(BaseOptimizer):
+
+    def __init__(self, trainable_layers):
+        BaseOptimizer.__init__(trainable_layers)
+
+    def initialize(self):
+        raise NotImplementedError
+
+    def update(self, learning_rate, w_grads, b_grads, step):
+        for layer in self.trainable_layers:
+            layer.update(dw=learning_rate * w_grads[layer],
+                         db=learning_rate * b_grads[layer])
