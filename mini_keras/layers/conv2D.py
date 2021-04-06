fa02a5a603721da0d4edb829d6d330dbcd5de200
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-06 12:29:45 +0000

    Fixed conv2D indentation error

--- mini_keras/layers/conv2D.py
+++ mini_keras/layers/conv2D.py
@@ -6,15 +6,15 @@ from mini_keras.layer import BaseLayer
 
 class Conv2D(BaseLayer):
 
-        def __init__(self, kernel_size, stride, n_c, padding='valid', activation=identity):
-            super().__init__()
-            self.kernel_size = kernel_size
-            self.stride = stride
-            self.padding = padding
-            self.pad = None
-            self.n_h, self.n_w, self.n_c = None, None, n_c
-            self.n_h_prev, self.n_w_prev, self.n_c_prev = None, None, None
-            self.weights = None
-            self.baises = None
-            self.activation = activation
-            self.cache = {}
+    def __init__(self, kernel_size, stride, n_c, padding='valid', activation=identity):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.pad = None
+        self.n_h, self.n_w, self.n_c = None, None, n_c
+        self.n_h_prev, self.n_w_prev, self.n_c_prev = None, None, None
+        self.weights = None
+        self.baises = None
+        self.activation = activation
+        self.cache = {}
