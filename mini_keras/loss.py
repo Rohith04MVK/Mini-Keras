4823bb9b4f66aa49ea56b7a52b22e77b11cd3079
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-13 10:22:05 +0000

    Changed a_last to y_pred in losses

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -6,12 +6,12 @@ from .base import BaseCostFunction
 
 
 class SoftmaxCrossEntropy(BaseCostFunction):
-    def f(self, a_last, y: t.Union[t.List, np.ndarray]):
+    def f(self, y_pred, y: t.Union[t.List, np.ndarray]):
         epsilon = 1e-20
         batch_size = y.shape[0]
-        cost = -1 / batch_size * (y * np.log(np.clip(a_last, epsilon, 1.0))).sum()
+        cost = -1 / batch_size * (y * np.log(np.clip(y_pred, epsilon, 1.0))).sum()
 
         return cost
 
-    def grad(self, a_last, y: t.Union[t.List, np.ndarray]) -> None:
+    def grad(self, y_pred, y: t.Union[t.List, np.ndarray]) -> None:
         raise NotImplementedError
