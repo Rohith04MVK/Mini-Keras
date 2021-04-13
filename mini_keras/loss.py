51eb200d83ead8385e2cc9a5f54c19cb499d4e6a
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-13 10:39:59 +0000

    Added grad function to softmax cross entropy

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -4,14 +4,14 @@ import numpy as np
 
 from .base import BaseCostFunction
 
+epsilon = 1e-20
 
 class SoftmaxCrossEntropy(BaseCostFunction):
     def f(self, y_pred, y: t.Union[t.List, np.ndarray]):
-        epsilon = 1e-20
         batch_size = y.shape[0]
         cost = -1 / batch_size * (y * np.log(np.clip(y_pred, epsilon, 1.0))).sum()
 
         return cost
 
     def grad(self, y_pred, y: t.Union[t.List, np.ndarray]) -> None:
-        raise NotImplementedError
+        return - np.divide(y, np.clip(y_pred, epsilon, 1.0))
