f0dd5a26d0f19a44243d016823ba2e585f90b4e5
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-14 09:30:16 +0000

    Started adding Sequential model

---
+++ mini_keras/models/sequential.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+import numpy as np
+
+from ..optimizer import gradient_descent
+
+
+class Sequential:
+    def __init__(self, input_dim, layers, cost_function, optimizer=gradient_descent, l2_lambda=0):
+        self.layers = layers
+        self.w_grads = {}
+        self.b_grads = {}
+        self.cost_function = cost_function
+        self.optimizer = optimizer
+        self.l2_lambda = l2_lambda
+
+        # Initialize the layers in the model providing the input dimension they should expect
+        self.layers[0].initialize(input_dim)
+        for prev_layer, curr_layer in zip(self.layers, self.layers[1:]):
+            curr_layer.initialize(prev_layer.get_output_dim())
+
+        self.trainable_layers = set(layer for layer in self.layers if layer.get_params() is not None)
+        self.optimizer = optimizer(self.trainable_layers)
+        self.optimizer.initialize()
