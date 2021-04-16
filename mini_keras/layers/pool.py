18f30c7481ea80d4bf0449f12ba88ec8b0f34389
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-16 07:45:23 +0000

    Started adding Pooling layer

---
+++ mini_keras/layers/pool.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from ..base import BaseLayer
+
+class Pool(BaseLayer):
+    def __init__(self, pool_size, stride, mode):
+        super().__init__()
+        self.pool_size = pool_size
+        self.stride = stride
+        self.n_h, self.n_w, self.n_c = None, None, None
+        self.n_h_prev, self.n_w_prev, self.n_c_prev = None, None, None
+        self.weights = None
+        self.biases = None
+        self.mode = mode
+        self.cache = {}
+
+    def initialize(self, in_dim):
+        self.n_h_prev, self.n_w_prev, self.n_c_prev = in_dim
+        self.n_h = int((self.n_h_prev - self.pool_size) / self.stride + 1)
+        self.n_w = int((self.n_w_prev - self.pool_size) / self.stride + 1)
+        self.n_c = self.n_c_prev
