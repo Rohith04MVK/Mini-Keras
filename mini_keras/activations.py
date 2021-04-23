0437a33167069ed2293105db974bd0acd095586f
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-23 14:11:49 +0000

    Fixed docs ofr activations

--- mini_keras/activations.py
+++ mini_keras/activations.py
@@ -14,11 +14,6 @@ class Identify(BaseActivation):
 
 
 class Sigmoid(BaseActivation):
-    """
-    Softmax converts a vector of values to a probability distribution.
-    The elements of the output vector are in range (0, 1) and sum to 1.
-    """
-
     def f(self, x):
         s = 1 / (1 + math.exp(-x))
         return s
@@ -50,6 +45,10 @@ class ReLU(BaseActivation):
 
 
 class SoftMax(BaseActivation):
+    """
+    Softmax converts a vector of values to a probability distribution.
+    The elements of the output vector are in range (0, 1) and sum to 1.
+    """
     def f(self, x):
         y = np.exp(x - np.max(x, axis=1, keepdims=True))
         return y / np.sum(y, axis=1, keepdims=True)
