38d5ef5f0f46c5072491756e15470eaa7507c448
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-25 15:54:55 +0000

    Added docs for GradientDescent Optimizer

--- mini_keras/optimizer.py
+++ mini_keras/optimizer.py
@@ -4,6 +4,12 @@ from .base import BaseOptimizer
 
 
 class GradientDescent(BaseOptimizer):
+    """
+    Gradient descent is the algorithm for finding a local minimum of a differentiable function.
+    The idea is to take repeated steps in the opposite direction of the
+    gradient (or approximate gradient) of the function at the current point,
+    because this is the direction of steepest descent.
+    """
     def __init__(self, trainable_layers) -> None:
         super().__init__(trainable_layers)
 
