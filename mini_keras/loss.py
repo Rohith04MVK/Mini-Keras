38abcbe776d0a0eb762d8b2f2d794380a968f8af
Rohith04MVK <007rohitjj04@gmail.com>
2021-04-25 15:41:45 +0000

    Added proper linting

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -15,6 +15,7 @@ class SoftmaxCrossEntropy(BaseCostFunction):
     ------
     The Softmax cross entropy error between the labels and the predictions
     """
+
     def f(self, y_pred, y: t.Union[t.List, np.ndarray]):
         batch_size = y.shape[0]
         cost = -1 / batch_size * (y * np.log(np.clip(y_pred, epsilon, 1.0))).sum()
