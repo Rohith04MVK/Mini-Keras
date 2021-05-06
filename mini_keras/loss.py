24767332cc7317c1ae1a2a695fa6b4d3e5185aed
Rohith <007rohitjj04@gmail.com>
2021-05-06 20:43:43 +0530

    Update loss.py

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -10,19 +10,17 @@ epsilon = 1e-20
 class SoftmaxCrossEntropy(BaseCostFunction):
     """
     Computes softmax cross entropy between `prediction` and `labels`.
-
     Return
     ------
     The Softmax cross entropy error between the labels and the predictions
     """
 
-    def f(self, a_last, y):
+    def f(self, y_pred, y: t.Union[t.List, np.ndarray]):
         batch_size = y.shape[0]
-        cost = -1 / batch_size * (y * np.log(np.clip(a_last, epsilon, 1.0))).sum()
+        cost = -1 / batch_size * (y * np.log(np.clip(y_pred, epsilon, 1.0))).sum()
+
         return cost
 
-    def grad(self, a_last, y):
-        return - np.divide(y, np.clip(a_last, epsilon, 1.0))
-
+    def grad(self, y_pred, y: t.Union[t.List, np.ndarray]) -> None:
+        return - np.divide(y, np.clip(y_pred, epsilon, 1.0))
     
-softmax_cross_entropy = SoftmaxCrossEntropy()
