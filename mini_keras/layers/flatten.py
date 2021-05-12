38a6ee64328a13c0850b7ae844c3389c6f86efea
Rohith <007rohitjj04@gmail.com>
2021-05-12 21:16:59 +0530

    fixed initialize

--- mini_keras/layers/flatten.py
+++ mini_keras/layers/flatten.py
@@ -14,14 +14,14 @@ class Flatten(BaseLayer):
         self.original_dim = None
         self.output_dim = None
 
-    def initialize(self, input_dims: t.Union[int, tuple]) -> None:
-        self.original_dim = input_dims
+    def initialize(self, in_dim):
+        self.original_dim = in_dim
         self.output_dim = reduce(lambda x, y: x * y, self.original_dim)
 
-    def forward(self, a_prev: np.ndarray, training: bool) -> np.ndarray:
+    def forward(self, a_prev, training):
         return a_prev.reshape(a_prev.shape[0], -1)
 
-    def backward(self, da: np.ndarray) -> tuple:
+    def backward(self, da):
         return da.reshape(da.shape[0], *self.original_dim), None, None
 
     def get_params(self):
@@ -30,5 +30,5 @@ class Flatten(BaseLayer):
     def update_params(self, dw, db):
         pass
 
-    def get_output_dim(self) -> None:
+    def get_output_dim(self):
         return self.output_dim
