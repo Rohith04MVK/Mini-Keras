93a9013f1574847aadc8e2f76a35fc2b911c5fc3
Rohith <007rohitjj04@gmail.com>
2021-05-14 20:43:57 +0530

    Added SigmoidCrossEntropy loss function

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -7,21 +7,28 @@ from .base import BaseCostFunction
 epsilon = 1e-20
 
 
-class SoftmaxCrossEntropy(BaseCostFunction):
-    """
-    Computes softmax cross entropy between `prediction` and `labels`.
-    Return
-    ------
-    The Softmax cross entropy error between the labels and the predictions
-    """
+class SigmoidCrossEntropy(CostFunction):
+    def f(self, a_last, y):
+        batch_size = y.shape[0]
+        # It would be better to have the logits and use this instead
+        # max(logits, 0) - logits * y + log(1 + exp(-abs(logits)))
+        a_last = np.clip(a_last, epsilon, 1.0 - epsilon)
+        cost = -1 / batch_size * (y * np.log(a_last) + (1 - y) * np.log(1 - a_last)).sum()
+        return cost
 
+    def grad(self, a_last, y):
+        a_last = np.clip(a_last, epsilon, 1.0 - epsilon)
+        return - (np.divide(y, a_last) - np.divide(1 - y, 1 - a_last))
+
+
+class SoftmaxCrossEntropy(CostFunction):
     def f(self, a_last, y):
         batch_size = y.shape[0]
         cost = -1 / batch_size * (y * np.log(np.clip(a_last, epsilon, 1.0))).sum()
         return cost
 
     def grad(self, a_last, y):
-        return -np.divide(y, np.clip(a_last, epsilon, 1.0))
-
+        return - np.divide(y, np.clip(a_last, epsilon, 1.0))
 
 softmax_cross_entropy = SoftmaxCrossEntropy()
+sigmoid_cross_entropy = SigmoidCrossEntropy()
