b76ee6d96ee0b841bc28deb7284ce5a7cf009cc8
Rohith <007rohitjj04@gmail.com>
2021-05-14 20:45:29 +0530

    Removed useless docs

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -10,8 +10,6 @@ epsilon = 1e-20
 class SigmoidCrossEntropy(CostFunction):
     def f(self, a_last, y):
         batch_size = y.shape[0]
-        # It would be better to have the logits and use this instead
-        # max(logits, 0) - logits * y + log(1 + exp(-abs(logits)))
         a_last = np.clip(a_last, epsilon, 1.0 - epsilon)
         cost = -1 / batch_size * (y * np.log(a_last) + (1 - y) * np.log(1 - a_last)).sum()
         return cost
