adece11a5ff2ceead9c1d5fd81fa4a6794f220c1
Rohith <007rohitjj04@gmail.com>
2021-05-14 20:50:09 +0530

    Fixed the Adam optimizer

--- mini_keras/optimizer.py
+++ mini_keras/optimizer.py
@@ -75,9 +75,9 @@ class RMSprop(BaseOptimizer):
             layer.update(dw, db)
 
 
-class Adam(BaseOptimizer):
+class Adam(Optimizer):
     def __init__(self, trainable_layers, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        BaseOptimizer.__init__(self, trainable_layers)
+        Optimizer.__init__(self, trainable_layers)
         self.v = {}
         self.s = {}
         self.beta1 = beta1
@@ -89,10 +89,10 @@ class Adam(BaseOptimizer):
             w, b = layer.get_params()
             w_shape = w.shape
             b_shape = b.shape
-            self.v[("dw", layer)] = np.zeros(w_shape)
-            self.v[("db", layer)] = np.zeros(b_shape)
-            self.s[("dw", layer)] = np.zeros(w_shape)
-            self.s[("db", layer)] = np.zeros(b_shape)
+            self.v[('dw', layer)] = np.zeros(w_shape)
+            self.v[('db', layer)] = np.zeros(b_shape)
+            self.s[('dw', layer)] = np.zeros(w_shape)
+            self.s[('db', layer)] = np.zeros(b_shape)
 
     def update(self, learning_rate, w_grads, b_grads, step):
         v_correction_term = 1 - np.power(self.beta1, step)
@@ -101,39 +101,23 @@ class Adam(BaseOptimizer):
         v_corrected = {}
 
         for layer in self.trainable_layers:
-            layer_dw = ("dw", layer)
-            layer_db = ("db", layer)
+            layer_dw = ('dw', layer)
+            layer_db = ('db', layer)
 
-            self.v[layer_dw] = (
-                self.beta1 * self.v[layer_dw] + (1 - self.beta1) * w_grads[layer]
-            )
-            self.v[layer_db] = (
-                self.beta1 * self.v[layer_db] + (1 - self.beta1) * b_grads[layer]
-            )
+            self.v[layer_dw] = (self.beta1 * self.v[layer_dw] + (1 - self.beta1) * w_grads[layer])
+            self.v[layer_db] = (self.beta1 * self.v[layer_db] + (1 - self.beta1) * b_grads[layer])
 
             v_corrected[layer_dw] = self.v[layer_dw] / v_correction_term
             v_corrected[layer_db] = self.v[layer_db] / v_correction_term
 
-            self.s[layer_dw] = self.beta2 * self.s[layer_dw] + (
-                1 - self.beta2
-            ) * np.square(w_grads[layer])
-            self.s[layer_db] = self.beta2 * self.s[layer_db] + (
-                1 - self.beta2
-            ) * np.square(b_grads[layer])
+            self.s[layer_dw] = (self.beta2 * self.s[layer_dw] + (1 - self.beta2) * np.square(w_grads[layer]))
+            self.s[layer_db] = (self.beta2 * self.s[layer_db] + (1 - self.beta2) * np.square(b_grads[layer]))
 
             s_corrected[layer_dw] = self.s[layer_dw] / s_correction_term
             s_corrected[layer_db] = self.s[layer_db] / s_correction_term
 
-            dw = (
-                learning_rate
-                * v_corrected[layer_dw]  # noqa: W503
-                / (np.sqrt(s_corrected[layer_dw]) + self.epsilon)  # noqa: W503
-            )
-            db = (
-                learning_rate
-                * v_corrected[layer_db]  # noqa: W503
-                / (np.sqrt(s_corrected[layer_db]) + self.epsilon)  # noqa: W503
-            )
+            dw = (learning_rate * v_corrected[layer_dw] / (np.sqrt(s_corrected[layer_dw]) + self.epsilon))
+            db = (learning_rate * v_corrected[layer_db] / (np.sqrt(s_corrected[layer_db]) + self.epsilon))
 
             layer.update_params(dw, db)
 
