f425b6c4785c0620a9ffc3212d7d247d8e906e7f
Rohith <007rohitjj04@gmail.com>
2021-05-15 17:44:58 +0530

    Completely overhauled the code base, removing abc temporarily, trying to fix everything

--- mini_keras/loss.py
+++ mini_keras/loss.py
@@ -1,15 +1,21 @@
-import typing as t
-
 import numpy as np
-
-from .base import BaseCostFunction
 
 epsilon = 1e-20
 
 
-class SigmoidCrossEntropy(BaseCostFunction):
+class CostFunction:
+    def f(self, a_last, y):
+        raise NotImplementedError
+
+    def grad(self, a_last, y):
+        raise NotImplementedError
+
+
+class SigmoidCrossEntropy(CostFunction):
     def f(self, a_last, y):
         batch_size = y.shape[0]
+        # It would be better to have the logits and use this instead
+        # max(logits, 0) - logits * y + log(1 + exp(-abs(logits)))
         a_last = np.clip(a_last, epsilon, 1.0 - epsilon)
         cost = -1 / batch_size * (y * np.log(a_last) + (1 - y) * np.log(1 - a_last)).sum()
         return cost
@@ -19,7 +25,7 @@ class SigmoidCrossEntropy(BaseCostFunction):
         return - (np.divide(y, a_last) - np.divide(1 - y, 1 - a_last))
 
 
-class SoftmaxCrossEntropy(BaseCostFunction):
+class SoftmaxCrossEntropy(CostFunction):
     def f(self, a_last, y):
         batch_size = y.shape[0]
         cost = -1 / batch_size * (y * np.log(np.clip(a_last, epsilon, 1.0))).sum()
@@ -27,6 +33,7 @@ class SoftmaxCrossEntropy(BaseCostFunction):
 
     def grad(self, a_last, y):
         return - np.divide(y, np.clip(a_last, epsilon, 1.0))
+
 
 softmax_cross_entropy = SoftmaxCrossEntropy()
 sigmoid_cross_entropy = SigmoidCrossEntropy()
