70f13c316c57d74c80b600d4075a05598c68eaf7
Rohith <007rohitjj04@gmail.com>
2021-05-15 17:42:59 +0530

    Completely overhauled the code base, removing abc temporarily, trying to fix everything

--- mini_keras/activations.py
+++ mini_keras/activations.py
@@ -1,9 +1,16 @@
+
 import numpy as np
 
-from .base import BaseActivation
+
+class ActivationFunction:
+    def f(self, x):
+        raise NotImplementedError
+
+    def df(self, x, cached_y=None):
+        raise NotImplementedError
 
 
-class Identify(BaseActivation):
+class Identity(ActivationFunction):
     def f(self, x):
         return x
 
@@ -11,13 +18,7 @@ class Identify(BaseActivation):
         return np.full(x.shape, 1)
 
 
-class Sigmoid(BaseActivation):
-    """
-    Applies the sigmoid activation function. For small values (<-5),
-    `sigmoid` returns a value close to zero, and for large values (>5)
-    the result of the function gets close to 1.
-    """
-
+class Sigmoid(ActivationFunction):
     def f(self, x):
         return np.where(x >= 0, 1 / (1 + np.exp(-x)), np.exp(x) / (1 + np.exp(x)))
 
@@ -26,19 +27,7 @@ class Sigmoid(BaseActivation):
         return y * (1 - y)
 
 
-class ReLU(BaseActivation):
-    """
-    Does the rectified linear unit function.
-    Returns 0 if the number is lesser than 0
-    Returns the number if the number is greater than 0
-    Example
-    -------
-    relu = mini_keras.activations.ReLU()
-    x = np.array([0.5, 10, -1, 5])
-    relu(x)
-    >>> [0.5 10.0 0.0 5.0]
-    """
-
+class ReLU(ActivationFunction):
     def f(self, x):
         return np.maximum(0, x)
 
@@ -46,15 +35,7 @@ class ReLU(BaseActivation):
         return np.where(x <= 0, 0, 1)
 
 
-class SoftMax(BaseActivation):
-    """
-    Softmax converts a vector of values to a probability distribution.
-    The elements of the output vector are in range (0, 1) and sum to 1.
-    Softmax is mostly used as the activation for the last
-    layer of a classification network to get the result as
-    a probability distribution.
-    """
-
+class SoftMax(ActivationFunction):
     def f(self, x):
         y = np.exp(x - np.max(x, axis=1, keepdims=True))
         return y / np.sum(y, axis=1, keepdims=True)
@@ -63,7 +44,7 @@ class SoftMax(BaseActivation):
         raise NotImplementedError
 
 
-identity = Identify()
+identity = Identity()
 sigmoid = Sigmoid()
 relu = ReLU()
 softmax = SoftMax()
