d3f6cbfc5d46088960d2c97566ac222a68c3fed5
Rohith <007rohitjj04@gmail.com>
2021-05-15 18:04:11 +0530

    Completely overhauled the code base, removing abc temporarily, trying to fix everything

--- mini_keras/layers/flatten.py
+++ mini_keras/layers/flatten.py
@@ -1,20 +1,23 @@
-import typing as t
 from functools import reduce
 
-import numpy as np
-
-from ..base import BaseLayer
+from src.layers.layer import Layer
 
 
-class Flatten(BaseLayer):
-    __slots__ = ("original_dim", "output_dim")
-
+class Flatten(Layer):
+    """Flatten layer.
+    Attributes
+    ----------
+    original_dim : tuple
+        Shape of the input ndarray.
+    output_dim : tuple
+        Shape of the output ndarray.
+    """
     def __init__(self):
         super().__init__()
         self.original_dim = None
         self.output_dim = None
 
-    def initialize(self, in_dim):
+    def init(self, in_dim):
         self.original_dim = in_dim
         self.output_dim = reduce(lambda x, y: x * y, self.original_dim)
 
