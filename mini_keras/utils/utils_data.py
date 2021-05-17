a1b395945ed0b5cc50bed168551b7856ada7b756
Rohith <007rohitjj04@gmail.com>
2021-05-17 10:55:42 +0530

    Fixed linting issues

--- mini_keras/utils/utils_data.py
+++ mini_keras/utils/utils_data.py
@@ -1,11 +1,13 @@
 import os
-import requests
+
 import numpy as np
+import requests
+
 
 def get_file(url, filepath, cache=True):
     file = requests.get(url)
-    file_ext = url.split("/") [-1]
-    if cache == True and os.path.exists(f"{os.path.join(filepath, file_ext)}"):
+    file_ext = url.split("/")[-1]
+    if cache is True and os.path.exists(f"{os.path.join(filepath, file_ext)}"):
         print("The file exists")
     else:
         open(f"{os.path.join(filepath, file_ext)}", 'wb').write(file.content)
