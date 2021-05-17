7585b8f941155c27657a2cfd7b269719024baed0
Rohith <007rohitjj04@gmail.com>
2021-05-17 15:54:26 +0530

    Fixed undefined variable error and added proper linting

--- mini_keras/utils/encoder.py
+++ mini_keras/utils/encoder.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 
-def one_hot_encoder():
+def one_hot_encoder(x, num_classes):
     out = np.zeros((x.shape[0], num_classes))
     out[np.arange(x.shape[0]), x[:, 0]] = 1
     return out
-  
